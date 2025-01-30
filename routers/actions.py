f990414487771370f1c6f45ae00536df11e6ebfa
Alan Freeman <afreeman@iwgvt.org>
2025-01-30 14:24:19 +0000

    static fixture 4 changed and working

--- routers/actions.py
+++ routers/actions.py
@@ -139,7 +139,7 @@ def get_json(session: Session = Depends(get_session))->ServerMessage:
     clear_table(session, Class_stock, 1)
     clear_table(session, User, 1)
 
-    for i in range(1,4):
+    for i in range(1,5):
         print("Loading table", i)
         load_table(session, Simulation, f"static/{i}/simulations.json", True, 1)
         load_table(session, SocialClass, f"static/{i}/classes.json", True, 1)
